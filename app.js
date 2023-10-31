5832e563a4fcaa022121f03e40daec3ed3dc4ef1
danaiangelidis <angelidisdanai@gmail.com>
2023-10-31 12:17:24 -0400

    added js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("Hello World");
+
+var http = require("http");
+const port = process.env.PORT || 3001;
+
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("Hello World!");
+  })
+  .listen(port);
